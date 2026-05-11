ddfd24906ec56a80fd881377356f774a41a2543a
taskurotta1 <mustonenmedia@gmail.com>
2026-05-11 12:59:49 +0300

    Worked with level and pasuemenu

---
+++ Don't look back/Assets/Scripts/Managers/PauseMenu.cs
@@ -0,0 +1,62 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public bool GameIsPaused = false;
+    public GameObject pauseMenuUI;
+    public GameObject controls;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        controls.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+
+    public void controlsUI()
+    {
+        pauseMenuUI.SetActive(false);
+        controls.SetActive(true);
+    }
+
+    public void ReturnPauseMenu()
+    {
+        controls.SetActive(false);
+        pauseMenuUI.SetActive(true);
+    }
+
+    public void ReturnMainMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
